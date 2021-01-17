7577a790cf28ed3084f6d3da99da11f4c048f364
PaulOnyirimba <Paul.Onyirimba@bryanuniversity.edu>
2021-01-17 15:11:07 -0500

    testing

---
+++ week4/index.js
@@ -0,0 +1,15 @@
+// url: https://api.vschool.io/paulonyirimba/todo
+// get all
+
+axios.get("https://api.vschool.io/PaulOnyirimba/todo")
+.then(response => {
+    for(let i = 0; i < response.data.length; i++){
+        const h1 = document.createElement('h1')
+        h1.textContent = response.data[i].title
+        document.body.appendChild(h1)
+    }
+})
+.catch(error => console.log(error))
+
+.then(response => console.log(response))
+    .catch(error => console.log(error))
