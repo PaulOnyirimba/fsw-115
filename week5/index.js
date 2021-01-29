8759fe193a808dab8702754c9a0e5133583e01a3
PaulOnyirimba <Onyirimbap@gmail.com>
2021-01-29 12:53:34 -0500

    testing

--- week5/index.js
+++ week5/index.js
@@ -21,6 +21,31 @@ axios.get("https://api.vschool.io/PaulOnyirimba/todo")
         const h1 = document.createElement('h1')
         h1.textContent = response.data[i].title
         document.body.appendChild(h1)
+        const input = document.createElement('input')
+        input.type = "checkbox"
+        document.body.appendChild(input)
+        const deleteitem = document.createElement('button')
+        deleteitem.textContent = "delete"
+        document.body.appendChild(deleteitem)
+        deleteitem.addEventListener("click", function(event){
+            event.preventDefault()
+            axios.delete("https://api.vschool.io/PaulOnyirimba/todo/" + response.data[i]._id)
+            .then(response => console.log(response.data))
+        .catch(error => console.log(error))
+
+        })
+        input.addEventListener("click", function(event){
+            event.preventDefault()
+            if(input.checked === true){
+                var box = {
+                    title: "My Second Todo",
+                description: "This is my second todo"
+                }
+            }
+            axios.put("https://api.vschool.io/PaulOnyirimba/todo/" + response.data[i]._id, box)
+            .then(response => console.log(response.data))
+        .catch(error => console.log(error))
+        })
     }
 })
 
