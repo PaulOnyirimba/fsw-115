ae34ac5e0d9da151a964ddee86b549f859757a1e
PaulOnyirimba <Onyirimbap@gmail.com>
2021-03-21 16:46:24 -0400

    testing

--- week4/index1.js
+++ week4/index1.js
@@ -1,27 +1,29 @@
 const button = document.getElementById('button1')
 
 function buttonClick() {
+    console.log("click")
     axios.get("https://opentdb.com/api.php?amount=4&category=14&difficulty=easy&type=boolean")
     .then(response => {
-        for (let i=0; i<response.data.length; i++) {
-           
+        console.log(response.data.results)
+        for (let i=0; i<response.data.results.length; i++) {
+           console.log(response.data.results[i])
             const div = document.createElement('div')
             document.getElementById('main').appendChild(div)
 
-            const h2 = document.createElement('h2')
-            h2.textContent = response.data[i].name
-            div.appendChild(h2)
+            // const h2 = document.createElement('h2')
+            // h2.textContent = response.data.results[i].name
+            // div.appendChild(h2)
 
             const p1 = document.createElement('p')
-            p1.textContent = 'question: ' + response.data[i].question
+            p1.textContent = 'question: ' + response.data.results[i].question
             div.appendChild(p1)
 
             const p2 = document.createElement('p')
-            p1.textContent = 'correct answer: ' + response.data[i].question
+            p2.textContent = 'correct answer: ' + response.data.results[i].question
             div.appendChild(p2)
 
             const p3 = document.createElement('p')
-            p1.textContent = 'incorrect answer: ' + response.data[i].question
+            p3.textContent = 'incorrect answer: ' + response.data.results[i].question
             div.appendChild(p3)
 
 
--- week4/index2.js
+++ week4/index2.js
@@ -3,25 +3,25 @@ const button = document.getElementById('button2')
 function buttonClick() {
     axios.get("https://opentdb.com/api.php?amount=4&category=21&difficulty=easy&type=boolean")
     .then(response => {
-        for (let i=0; i<response.data.length; i++) {
+        for (let i=0; i<response.data.results.length; i++) {
            
             const div = document.createElement('div')
             document.getElementById('main').appendChild(div)
 
-            const h2 = document.createElement('h2')
-            h2.textContent = response.data[i].name
-            div.appendChild(h2)
+            // const h2 = document.createElement('h2')
+            // h2.textContent = response.data[i].name
+            // div.appendChild(h2)
 
             const p1 = document.createElement('p')
-            p1.textContent = 'question: ' + response.data[i].question
+            p1.textContent = 'question: ' + response.data.results[i].question
             div.appendChild(p1)
 
             const p2 = document.createElement('p')
-            p1.textContent = 'correct answer: ' + response.data[i].question
+            p2.textContent = 'correct answer: ' + response.data.results[i].question
             div.appendChild(p2)
 
             const p3 = document.createElement('p')
-            p1.textContent = 'incorrect answer: ' + response.data[i].question
+            p3.textContent = 'incorrect answer: ' + response.data.results[i].question
             div.appendChild(p3)
 
 
